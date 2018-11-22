95d4e714f6536748ea2b84d6f1417a9de80fbe01
Nikx Fabrizio <w3c.nadeem@hotmail.com>
2018-11-22 16:48:53 +0530

    Setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const world = '🗺️';
+
+export function hello(word: string = world): string {
+  return `Hello ${world}! `;
+}
+
+export class Fruit{
+    constructor(){
+
+    }
+
+    test(){
+        return 'test!';
+    }
+}
